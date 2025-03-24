4e5c1e095660724e4c4c55f437e887bce775ea0d
Yugank Bansal <yugankbansal.00001276033.aesl@gmail.com>
2025-03-24 19:44:01 +0530

    bugs fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-instrument-sans)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
